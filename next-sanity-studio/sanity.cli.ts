34127c661efd1a15dc9151a09c77760fbe35c053
Breno Ocanha <breno@dmxdesign.com.br>
2023-09-03 18:23:38 -0300

    project

--- next-sanity-studio/sanity.cli.ts
+++
@@ -1,8 +0,0 @@
-import {defineCliConfig} from 'sanity/cli'
-
-export default defineCliConfig({
-  api: {
-    projectId: 's7dj86x9',
-    dataset: 'production'
-  }
-})
---
+++ sanity/config/client-config.ts
@@ -0,0 +1,7 @@
+const config = {
+  projectId: "s7dj86x9",
+  dataset: "production",
+  apiVersion: "2023-03-09",
+}
+
+export default config;
